107860f1f3b7e37a009873b0b930816fde887aea
phamkhanhtrang <144437362+phamkhanhtrang@users.noreply.github.com>
2023-11-21 00:19:16 +0700

    Add files via upload

---
+++ Lab8/Lab8/Bai1.php
@@ -0,0 +1,40 @@
+<?php
+require 'PHPMailer-master/src/PHPMailer.php';  //nhúng thư viện vào để dùng, sửa lại đường dẫn cho đúng nếu bạn lưu vào chỗ khác
+require "PHPMailer-master/src/SMTP.php"; //nhúng thư viện vào để dùng
+require 'PHPMailer-master/src/Exception.php'; //nhúng thư viện vào để dùng
+$mail = new PHPMailer\PHPMailer\PHPMailer(true);  //true: enables exceptions
+try {
+    // $mail->SMTPDebug = 2;  // 0,1,2: chế độ debug. khi mọi cấu hình đều tớt thì chỉnh lại 0 nhé
+    $mail->isSMTP();
+    $mail->CharSet  = "utf-8";
+    $mail->Host = 'smtp.gmail.com';  //SMTP servers
+    $mail->SMTPAuth = true; // Enable authentication
+    $nguoigui = 'trangpk.22it@vku.udn.vn';
+    $matkhau = 'bhwr hveq sprc nzog'; // mật khẩu của tài khoản 
+    $tennguoigui = 'Khánh Trang';
+    $mail->Username = $nguoigui; // SMTP username
+    $mail->Password = $matkhau;   // SMTP password
+    $mail->SMTPSecure = 'ssl';  // encryption TLS/SSL 
+    $mail->Port = 465;  // port to connect to                
+    $mail->setFrom($nguoigui, $tennguoigui);
+    $to = "khanhtrang123@gmail.com";
+    $to_name = "Trang";
+
+    $mail->addAddress($to, $to_name); //mail và tên người nhận
+    $mail->isHTML(true);  // Set email format to HTML
+    $mail->Subject = 'Gửi thư từ Thông Thảo';
+    $noidungthu = "<b>Hiii!</b><br>chào cọu!";
+    $mail->Body = $noidungthu;
+    $mail->AddAttachment("4.png", "picture");
+    $mail->smtpConnect(array(
+        "ssl" => array(
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+            "allow_self_signed" => true
+        )
+    ));
+    $mail->send();
+    echo 'Đã gửi mail xong';
+} catch (Exception $e) {
+    echo 'Mail không gửi được. Lỗi: ', $mail->ErrorInfo;
+}
---
+++ Lab8/Lab8/Bai2/index.php
@@ -0,0 +1,7 @@
+<form action="mail.php" enctype="multipart/form-data" method="POST">
+<input type="text" class="form-control" name="email" placeholder="Email"><br>
+<input type="text" class="form-control" name="tieude" placeholder="ten"><br>
+<textarea name="content" id="editor" class="form-control"></textarea><br>
+<input type="file" class="form-control" name="file"  ><br>
+<button type="submit" class="btn btn-primary">Gửi</button>
+
